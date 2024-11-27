5db1ef9c5926770d623aceb9f583c1a3d7f48c46
kaln27 <liaojuncheng123@foxmail.com>
2024-11-27 10:24:08 +0800

    Support multi GPUs training in a4

--- a4/src/run.py
+++ a4/src/run.py
@@ -10,78 +10,22 @@ import torch
 from tqdm import tqdm
 from torch.utils.tensorboard import SummaryWriter
 
-random.seed(0)
+import torch.multiprocessing as mp
+from torch.distributed import init_process_group, destroy_process_group
+import os
 
-argp = argparse.ArgumentParser()
-argp.add_argument('function', help="Choose pretrain, finetune, or evaluate")
-argp.add_argument('variant', help="Choose vanilla or rope")
-argp.add_argument('pretrain_corpus_path', default=None)
-argp.add_argument('--reading_params_path',default=None)
-argp.add_argument('--writing_params_path',default=None)
-argp.add_argument('--finetune_corpus_path', default=None)
-argp.add_argument('--eval_corpus_path', default=None)
-argp.add_argument('--outputs_path', default=None)
-argp.add_argument('--pretrain_lr', default=6e-3, type=float)
-argp.add_argument('--finetune_lr', default=6e-4, type=float)
-argp.add_argument('--tb_expt_name', help='debug string for tb log.',
-                  default='run')
-args = argp.parse_args()
+def ddp_setup(rank: int, world_size: int):
+   """
+   Args:
+       rank: Unique identifier of each process
+      world_size: Total number of processes
+   """
+   os.environ["MASTER_ADDR"] = "localhost"
+   os.environ["MASTER_PORT"] = "12355"
+   torch.cuda.set_device(rank)
+   init_process_group(backend="nccl", rank=rank, world_size=world_size)
 
-device = 'cpu'
-if torch.cuda.is_available():
-    device = torch.cuda.current_device()
-elif torch.backends.mps.is_available() and args.variant == 'vanilla':
-    device = 'mps'
-
-# TensorBoard training log
-writer = SummaryWriter(log_dir='expt/%s/%s_%s_pt_lr_%f_ft_lr_%f' % (
-    args.function,
-    args.tb_expt_name,
-    args.variant,
-    args.pretrain_lr,
-    args.finetune_lr))
-
-# Keep the block size 128
-# Why is the pretraining corpus always required (even if we're not pretraining?)
-# It's because we're using it as a hack to always have the same vocabulary
-# (that is, the same mapping from character to integer, and we build the
-# vocab from the pretraining corpus.)
-block_size = 128
-text = open(args.pretrain_corpus_path, encoding='utf-8').read()
-pretrain_dataset = dataset.CharCorruptionDataset(text, block_size)
-
-# We don't suggest you change these hyperparameters, as they're known to work.
-# use them for both the vanilla and the RoPE models
-mconf = models.GPTConfig(
-    pretrain_dataset.vocab_size,
-    pretrain_dataset.block_size,
-    n_layer=4,
-    n_head=8,
-    n_embd=256)
-
-# define models.
-# note: models should moved to device defined on lines 30-34.
-
-model = None
-if args.variant == 'vanilla':
-    # TODO: [part c] Make some model here
-    ### YOUR CODE HERE ###
-    model = models.GPT(mconf).to(device)
-    ### END YOUR CODE ###
-elif args.variant == 'rope':
-    # TODO: [part g] Make some other model here
-    # set mconf.rope parameter
-    ### YOUR CODE HERE ###
-    mconf.rope = True
-    model = models.GPT(mconf).to(device)
-    ### END YOUR CODE ###
-else:
-    raise ValueError("Unknown model variant")
-
-print('Model on device: ', next(model.parameters()).device)
-
-# Perform pretraining, finetuning, or evaluation
-if args.function == 'pretrain':
+def pretrain(model, pretrain_dataset, args):
     assert args.writing_params_path is not None
     # TODO [part f]:
     # - Given:
@@ -104,13 +48,14 @@ if args.function == 'pretrain':
 
     ### YOUR CODE HERE ###
     tconf = trainer.TrainerConfig(max_epochs=650, batch_size=128, learning_rate=args.finetune_lr,
-                      lr_decay=True, warmup_tokens=512*20, final_tokens=650*len(pretrain_dataset)*block_size,
-                      num_workers=4, writer=writer)
-    trainer = trainer.Trainer(model, pretrain_dataset, None, tconf)
-    trainer.train()
-    torch.save(model.state_dict(), args.writing_params_path)
+                    lr_decay=True, warmup_tokens=512*20, final_tokens=650*len(pretrain_dataset)*args.block_size,
+                    num_workers=4, writer=args.writer, ckpt_path=args.writing_params_path, device=args.device)
+    trainer_ = trainer.Trainer(model, pretrain_dataset, None, tconf)
+    trainer_.train()
+    trainer_.save_checkpoint()
     ### END YOUR CODE ###
-elif args.function == 'finetune':
+
+def finetune(model, pretrain_dataset, args):
     assert args.writing_params_path is not None
     assert args.finetune_corpus_path is not None
     # TODO [part c] [part f]:
@@ -154,13 +99,15 @@ elif args.function == 'finetune':
     finetune_data = open(args.finetune_corpus_path, encoding='utf-8').read()
     finetune_dataset = dataset.NameDataset(pretrain_dataset, finetune_data)
     tconf = trainer.TrainerConfig(max_epochs=10, batch_size=256, learning_rate=args.finetune_lr,
-                      lr_decay=True, warmup_tokens=512*20, final_tokens=200*len(pretrain_dataset)*block_size,
-                      num_workers=4, writer=writer)
-    trainer = trainer.Trainer(model, finetune_dataset, None, tconf)
-    trainer.train()
-    torch.save(model.state_dict(), args.writing_params_path)
+                    lr_decay=True, warmup_tokens=512*20, final_tokens=200*len(pretrain_dataset)*args.block_size,
+                    num_workers=4, writer=args.writer, ckpt_path=args.writing_params_path, device=args.device)
+    trainer_ = trainer.Trainer(model, finetune_dataset, None, tconf)
+    trainer_.train()
+    trainer_.save_checkpoint()
     ### END YOUR CODE ###
-elif args.function == 'evaluate':
+
+def evaluate(model, pretrain_dataset, args):
+    if args.device != 0: return
     assert args.outputs_path is not None
     assert args.reading_params_path is not None
     assert args.eval_corpus_path is not None
@@ -173,7 +120,7 @@ elif args.function == 'evaluate':
             x = line.split('\t')[0]
             x = x + '⁇'
             x = torch.tensor([pretrain_dataset.stoi[s] for s in x],
-                             dtype=torch.long)[None,...].to(device)
+                            dtype=torch.long)[None,...].to(args.device)
             pred = utils.sample(model, x, 32, sample=False)[0]
             completion = ''.join([pretrain_dataset.itos[int(i)] for i in pred])
             pred = completion.split('⁇')[1]
@@ -184,3 +131,89 @@ elif args.function == 'evaluate':
         print(f'Correct: {correct} out of {total}: {correct/total*100}%')
     else:
         print(f'Predictions written to {args.outputs_path}; no targets provided')
+
+def task(rank, args):
+    ddp_setup(rank, args.world_size)
+    device = rank
+
+    writer = SummaryWriter(log_dir='expt/%s/%s_%s_pt_lr_%f_ft_lr_%f' % (
+        args.function,
+        args.tb_expt_name,
+        args.variant,
+        args.pretrain_lr,
+        args.finetune_lr))
+
+    # Keep the block size 128
+    # Why is the pretraining corpus always required (even if we're not pretraining?)
+    # It's because we're using it as a hack to always have the same vocabulary
+    # (that is, the same mapping from character to integer, and we build the
+    # vocab from the pretraining corpus.)
+    block_size = 128
+    text = open(args.pretrain_corpus_path, encoding='utf-8').read()
+    pretrain_dataset = dataset.CharCorruptionDataset(text, block_size)
+
+    args.block_size = block_size
+    args.device = device
+    args.writer = writer
+
+    # We don't suggest you change these hyperparameters, as they're known to work.
+    # use them for both the vanilla and the RoPE models
+    mconf = models.GPTConfig(
+        pretrain_dataset.vocab_size,
+        pretrain_dataset.block_size,
+        n_layer=4,
+        n_head=8,
+        n_embd=256)
+
+    # define models.
+    # note: models should moved to device defined on lines 30-34.
+
+    model = None
+    if args.variant == 'vanilla':
+        # TODO: [part c] Make some model here
+        ### YOUR CODE HERE ###
+        model = models.GPT(mconf).to(device)
+        ### END YOUR CODE ###
+    elif args.variant == 'rope':
+        # TODO: [part g] Make some other model here
+        # set mconf.rope parameter
+        ### YOUR CODE HERE ###
+        mconf.rope = True
+        model = models.GPT(mconf).to(device)
+        ### END YOUR CODE ###
+    else:
+        raise ValueError("Unknown model variant")
+
+    print('Model on device: ', next(model.parameters()).device)
+
+    # Perform pretraining, finetuning, or evaluation
+    if args.function == 'pretrain':
+        pretrain(model, pretrain_dataset, args)
+    elif args.function == 'finetune':
+        finetune(model, pretrain_dataset, args)
+    elif args.function == 'evaluate':
+        evaluate(model, pretrain_dataset, args)
+    
+    destroy_process_group()
+
+if __name__ == '__main__':
+    random.seed(0)
+
+    argp = argparse.ArgumentParser()
+    argp.add_argument('function', help="Choose pretrain, finetune, or evaluate")
+    argp.add_argument('variant', help="Choose vanilla or rope")
+    argp.add_argument('pretrain_corpus_path', default=None)
+    argp.add_argument('--reading_params_path',default=None)
+    argp.add_argument('--writing_params_path',default=None)
+    argp.add_argument('--finetune_corpus_path', default=None)
+    argp.add_argument('--eval_corpus_path', default=None)
+    argp.add_argument('--outputs_path', default=None)
+    argp.add_argument('--pretrain_lr', default=6e-3, type=float)
+    argp.add_argument('--finetune_lr', default=6e-4, type=float)
+    argp.add_argument('--tb_expt_name', help='debug string for tb log.',
+                    default='run')
+    args = argp.parse_args()
+    
+    args.world_size = torch.cuda.device_count()
+
+    mp.spawn(task, args=(args,), nprocs=args.world_size)
--- a4/src/trainer.py
+++ a4/src/trainer.py
@@ -14,6 +14,10 @@ import torch.optim as optim
 from torch.optim.lr_scheduler import LambdaLR
 from torch.utils.data.dataloader import DataLoader
 
+from torch.utils.data.distributed import DistributedSampler
+from torch.nn.parallel import DistributedDataParallel as DDP
+
+
 logger = logging.getLogger(__name__)
 
 class TrainerConfig:
@@ -29,6 +33,8 @@ class TrainerConfig:
     warmup_tokens = 375e6 # these two numbers come from the GPT-3 paper, but may not be good defaults elsewhere
     final_tokens = 260e9 # (at what point we reach 10% of original LR)
     # checkpoint settings
+    device = None
+    save_every = None
     ckpt_path = None
     num_workers = 0 # for DataLoader
     writer = None
@@ -37,52 +43,46 @@ class TrainerConfig:
         for k,v in kwargs.items():
             setattr(self, k, v)
 
-class Trainer:
+def get_optimizer(model, config: "TrainerConfig"):
+    # create the optimizer
+    no_decay = ["bias", "LayerNorm.weight"]
+    params_decay = [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)]
+    params_nodecay = [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)]
+    optim_groups = [
+        {"params": params_decay, "weight_decay": config.weight_decay},
+        {"params": params_nodecay, "weight_decay": 0.0},
+    ]
+    optimizer = optim.AdamW(optim_groups, lr=config.learning_rate, betas=config.betas)
+    return optimizer
 
-    def __init__(self, model, train_dataset, test_dataset, config):
-        self.model = model
+class Trainer:
+    def __init__(self, model, train_dataset, test_dataset, config: 'TrainerConfig'):
+        # take over whatever gpus are on the system
+        self.model = DDP(model, device_ids=[config.device])
+        self.optimizer = get_optimizer(model, config)
+        self.device = config.device
         self.train_dataset = train_dataset
         self.test_dataset = test_dataset
         self.config = config
 
-        # take over whatever gpus are on the system
-        self.device = 'cpu'
-        if torch.cuda.is_available():
-            self.device = torch.cuda.current_device()
-            self.model = torch.nn.DataParallel(self.model).to(self.device)
-        elif torch.backends.mps.is_available() and not self.model.rope:
-            self.device = 'mps'
-            self.model = self.model.to(self.device)
-
     def save_checkpoint(self):
-        if self.config.ckpt_path is not None:
+        if self.config.ckpt_path is not None and self.device == 0:
             ckpt_model = self.model.module if hasattr(self.model, "module") else self.model
             logger.info("saving %s", self.config.ckpt_path)
             torch.save(ckpt_model.state_dict(), self.config.ckpt_path)
 
     def train(self):
         model, config = self.model, self.config
-
-        # create the optimizer
-        no_decay = ["bias", "LayerNorm.weight"]
-        params_decay = [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)]
-        params_nodecay = [p for n, p in model.named_parameters() if any(nd in n for nd in no_decay)]
-        optim_groups = [
-            {"params": params_decay, "weight_decay": config.weight_decay},
-            {"params": params_nodecay, "weight_decay": 0.0},
-        ]
-        optimizer = optim.AdamW(optim_groups, lr=config.learning_rate, betas=config.betas)
         step = 0
-        def run_epoch(split):
-            nonlocal step
-            is_train = split == 'train'
-            model.train(is_train)
-            data = self.train_dataset if is_train else self.test_dataset
-            loader = DataLoader(data, batch_size=config.batch_size, num_workers=config.num_workers)
 
+        def run_epoch(split, loader, epoch):
+            nonlocal step
+
+            is_train = split == 'train'
             losses = []
-            pbar = tqdm(enumerate(loader), total=len(loader)) if is_train else enumerate(loader)
-            
+            pbar = tqdm(enumerate(loader), total=len(loader))
+            loader.sampler.set_epoch(epoch)
+
             for it, (x, y) in pbar:
 
                 # place data on the correct device
@@ -98,10 +98,10 @@ class Trainer:
                 if is_train:
 
                     # backprop and update the parameters
-                    model.zero_grad()
+                    self.optimizer.zero_grad()
                     loss.backward()
                     torch.nn.utils.clip_grad_norm_(model.parameters(), config.grad_norm_clip)
-                    optimizer.step()
+                    self.optimizer.step()
 
                     # decay the learning rate based on our progress
                     if config.lr_decay:
@@ -114,13 +114,13 @@ class Trainer:
                             progress = float(self.tokens - config.warmup_tokens) / float(max(1, config.final_tokens - config.warmup_tokens))
                             lr_mult = max(0.1, 0.5 * (1.0 + math.cos(math.pi * progress)))
                         lr = config.learning_rate * lr_mult
-                        for param_group in optimizer.param_groups:
+                        for param_group in self.optimizer.param_groups:
                             param_group['lr'] = lr
                     else:
                         lr = config.learning_rate
 
                     # report progress
-                    pbar.set_description(f"epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}")
+                    pbar.set_description(f"[{model.device}] epoch {epoch+1} iter {it}: train loss {loss.item():.5f}. lr {lr:e}")
                     
                     if config.writer is not None:
                         config.writer.add_scalar('train/loss',  loss.item(), step)
@@ -130,11 +130,19 @@ class Trainer:
             if not is_train:
                 logger.info("test loss: %f", np.mean(losses))
 
+        train_loader = DataLoader(self.train_dataset, batch_size=config.batch_size, 
+                            num_workers=config.num_workers, sampler=DistributedSampler(self.train_dataset),
+                            pin_memory=True, persistent_workers=True)
+        test_loader = DataLoader(self.test_dataset, batch_size=config.batch_size, 
+                            num_workers=config.num_workers, sampler=DistributedSampler(self.test_dataset),
+                            pin_memory=True, persistent_workers=True) if self.test_dataset else None
         self.tokens = 0 # counter used for learning rate decay
         for epoch in range(config.max_epochs):
+            model.train()
+            run_epoch('train', train_loader, epoch)
+            if test_loader is not None:
+                model.eval()
+                run_epoch('test', test_loader, epoch)
 
-            run_epoch('train')
-            if self.test_dataset is not None:
-                run_epoch('test')
-
-            self.save_checkpoint()
+            if self.config.save_every is not None and epoch % self.config.save_every == 0:
+                self.save_checkpoint()
